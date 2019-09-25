b3cabfda1e546b8aaa9412469468fcf72b35111d
Hollwill <22golomonzin@gmail.com>
2019-09-25 16:22:03 +0700

    Added settings

--- manage.py
+++ manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), 'local_settings.py')):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings.development")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
